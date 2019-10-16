0f7df1df30987ba5f5ce1c5bd93606f41d0de895
xrw560 <ncutits@163.com>
2019-10-16 22:59:59 +0800

    keras-CNN

---
+++ deep_learning/keras_demo/CNN_Conv2D/CNN_Conv2d_predict.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+### Prediction
+
+import numpy as np
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import matplotlib.image as processimage
+
+# load trained model
+model = load_model('model_name.h5')
+
+
+class MainPredictImg(object):
+    def __init__(self):
+        pass
+
+    def pred(self, filename):
+        # np array
+        pred_img = processimage.imread(filename)  # read image
+        pred_img = np.array(pred_img)  # tranfer to array np
+        pred_img = pred_img.reshape(-1, 28, 28, 1)  # reshape into network needed shape
+        prediction = model.predict(pred_img)  # predict
+        Final_prediction = [result.argmax() for result in prediction][0]  # [1,2,3,1,3,2,2,]
+        a = 0
+        for i in prediction[0]:
+            print(a)
+            print('Percent:{:.30%}'.format(i))
+            a = a + 1
+
+        return Final_prediction
+
+
+def main():
+    predict = MainPredictImg()
+    res = predict.pred('Predict_image/7.jpg')
+    print("your number is:-->", res)
+
+
+if __name__ == '__main__':
+    main()
